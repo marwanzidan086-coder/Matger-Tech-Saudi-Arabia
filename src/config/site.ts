5e77fed4c6f3978761c35e54a959e82f938e12d0
unknown <marwanzidan086@gmail.com>
2025-10-05 04:49:41 +0300

    Add Google site verification tag

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,9 +1,6 @@
-
 export const siteConfig = {
-    name: 'Matger.tech',
+    name: 'متجر تك',
     description: 'متجر إلكتروني متكامل لبيع أحدث المنتجات التقنية.',
-    whatsappNumbers: ['+201557219572'],
+    whatsappNumbers: ['+201557219572'], // <-- هنا التعديل
     shippingCost: 25,
 };
-  
-export type SiteConfig = typeof siteConfig;
